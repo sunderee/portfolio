e73670cc99e6d7ac95d411041d8f2679f105ba17
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2025-03-31 16:16:21 +0200

    Introduce ShadCN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,7 @@ const roboto = Roboto({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Peter Aleksander Bizjak",
-  description:
-    "Professional portfolio of Peter Aleksander Bizjak, offering software development, cybersecurity services, and technical consulting.",
+  description: "Professional portfolio of Peter Aleksander Bizjak, offering software development, cybersecurity services, and technical consulting.",
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
